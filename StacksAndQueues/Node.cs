355567379f0d0ad36b550f715cb4b62cdb28380b
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-06 20:26:57 +0530

    [Cheatn]Add. Creating Stack using Linked list and pushing elements.

---
+++ StacksAndQueues/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Node
+    {
+        ///Variables.
+        public int data;
+        public Node next;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
