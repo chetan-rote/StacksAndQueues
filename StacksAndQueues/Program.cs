736e5ab8aaaf0a885b0f756b6858126aca0d5e3d
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-06 21:22:15 +0530

    [Chetan]Add. Dequeue(Delete) elements from queue.

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -11,26 +11,32 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             ///UC1-Creating stack and pushing elements.
-            //Console.WriteLine("Welcome to Data Structures Stack and Queue.");
-            //StackLinkedList stack = new StackLinkedList();
-            //stack.Push(70);
-            //stack.Push(30);
-            //stack.Push(56);
-            //stack.Display();
+            Console.WriteLine("Welcome to Data Structures Stack and Queue.");
+            StackLinkedList stack = new StackLinkedList();
+            Console.WriteLine("****Stack****");
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
             ///UC2 Ability to peak and pop from stack.
-            //stack.Size();
-            //stack.Peak();
-            //stack.Pop();
-            //Console.WriteLine("Stack :");
-            //stack.Display();
-            //stack.Size();
-
+            stack.Size();
+            stack.Peak();
+            stack.Pop();
+            Console.WriteLine("Stack :");
+            stack.Display();
+            stack.Size();
             ///UC3 Creating Queue Using linked list.
             QueueLinkedList queue = new QueueLinkedList();
+            Console.WriteLine("****Queue****");
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
             queue.Display();
+            queue.Size();
+            ///UC4 Dequeue elements.
+            queue.Dequeue();
+            queue.Display();
+            queue.Size();
         }
     }
 }
--- StacksAndQueues/QueueLinkedList.cs
+++ StacksAndQueues/QueueLinkedList.cs
@@ -31,6 +31,22 @@ namespace StacksAndQueues
             Console.WriteLine(node.data + " inserted into Queue");
         }
         /// <summary>
+        /// UC-4
+        /// Deletes the element from queue.
+        /// </summary>
+        public void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Console.WriteLine(head.data + " removed from Queue");
+                head = head.next;
+            }
+        }
+        /// <summary>
         /// Displays the element from queue.
         /// </summary>
         public void Display()
@@ -49,6 +65,24 @@ namespace StacksAndQueues
                 }
                 Console.Write(n.data + "\n");
             }
+        }
+        /// <summary>
+        /// Gives size of queue.
+        /// </summary>
+        public void Size()
+        {
+            int count = 0;
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            Console.WriteLine("Size :" + count);
         }
     }
 }
